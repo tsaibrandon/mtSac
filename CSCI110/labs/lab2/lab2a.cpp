796b0bcfe60d96abf36fb034aa8ac088f0a4cbbb
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-01-19 23:52:58 -0800

    lab 2a completed

---
+++ CSCI110/labs/lab2/lab2a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    string answer = "y";
+    
+    while (answer == "y") {
+        const int PACKAGE_RETAIL = 99.00;
+        int packageAmt;
+        int priceBase;
+        double discount;
+        double newPrice;
+
+        cout << fixed << setprecision(2);
+
+        cout << "How many packages did you purchase?" << "\n";
+        cin >> packageAmt;
+
+        priceBase = PACKAGE_RETAIL * packageAmt;
+
+        if (packageAmt < 10) {
+            cout << "\n" << "You don't qualify for a discount." << "\n";
+            cout << "Total: $" << priceBase << "\n";
+        } else if (packageAmt >= 10 && packageAmt <= 19) {
+            discount = priceBase * 0.2;
+            newPrice = priceBase - (discount);
+            cout << "\n" << "You qualify for a 20% discount!" << "\n";
+            cout << "Total: $" << newPrice << "\n";
+        } else if (packageAmt >= 20 && packageAmt <= 49) {
+            discount = priceBase * 0.3;
+            newPrice = priceBase - (discount);
+            cout << "\n" << "You qualify for a 30% discount!" << "\n";
+            cout << "Total: $" << newPrice << "\n";
+        } else if (packageAmt >= 50 && packageAmt <= 99) {
+            discount = priceBase * 0.4;
+            newPrice = priceBase - (discount);
+            cout << "\n" << "You qualify for a 40% discount!" << "\n";
+            cout << "Total: $" << newPrice << "\n";
+        } else if (packageAmt >= 100) {
+            discount = priceBase * 0.5;
+            newPrice = priceBase - (discount);
+            cout << "\n" << "You qualify for a 50% discount!" << "\n";
+            cout << "Total: $" << newPrice << "\n";
+        }
+
+        cout << "\n" << "Would you like to enter a different amount of packages? (y/n)" << "\n";
+        cin >> answer;
+
+        cout << "\n";
+    }
+
+    return 0;
+}
