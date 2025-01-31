e423362f32b076bd61b81f09c5be399f5303a06e
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-01-30 22:22:46 -0800

    added the prologue and fixed up lab 3

--- CSCI110/labs/lab3/lab3a.cpp
+++ CSCI110/labs/lab3/lab3a.cpp
@@ -1,3 +1,13 @@
+/*
+Prolog
+    a. Program Description: Paint Job Estimator. This program calculates the total amount of materials and costs of what is needed for the paint job.
+    b. Author: Brandon Tsai
+    c. Date/Time: 01/30/2025
+    d. Input Variables: square_feet, gallon_price
+    e. Process Flow: The program asks the user to input square_feet and then the gallon_price. It then finds the initial_wall by square_feet / BASE_WALL, finds the total_paint_gallon by BASE_PAINT * initial wall, then finds total_labor_hour by BASE_LABOR_HOUR * initial_wall then finds total_paint_cost by total_paint_gallon * gallon_price, then finds total_labor_cost by BASE_LABOR_COST * total_labor_hour, and finally finds total_cost by total_paint_cost + total_labor_cost. Then it prints out all the totals.
+    f. Output Variables: total_paint_gallon, total_labor_hour, total_paint_cost, total_labor_cost, total_cost
+*/
+
 #include <iostream>
 #include <iomanip>
 
@@ -5,29 +15,38 @@ using namespace std;
 
 void handle_winch(int sig) {}
 
+void get_wall_space(double& square_feet){
+    cout << "Enter the total wall space in square feet: ";
+    cin >> square_feet;
+    return;
+}
+
+void get_gallon_price(double& gallon_price) {
+    cout << "Enter the cost for a gallon of paint: $";
+    cin >> gallon_price;
+    return;
+}
+ 
 int main() {
     signal(SIGWINCH, handle_winch);
 
-    const int BASE_WALL = 115;
-    const int BASE_PAINT = 1;
-    const int BASE_LABOR_HOUR = 8;
+    const double BASE_WALL = 115;
+    const double BASE_PAINT = 1;
+    const double BASE_LABOR_HOUR = 8;
     const double BASE_LABOR_COST = 20;
-    int square_feet;
+    double square_feet;
     double gallon_price;
-    int initial_wall;
-    int total_paint_gallon;
-    int total_labor_hour;
+    double initial_wall;
+    double total_paint_gallon;
+    double total_labor_hour;
     double total_paint_cost;
     double total_labor_cost;
     double total_cost;
 
     cout << fixed << setprecision(2);
     
-    cout << "Enter the total wall space in square feet: ";
-    cin >> square_feet;
-
-    cout << "Enter the cost for a gallon of paint: $";
-    cin >> gallon_price;
+    get_wall_space(square_feet);
+    get_gallon_price(gallon_price);
 
     initial_wall = square_feet / BASE_WALL;
     total_paint_gallon = BASE_PAINT * initial_wall;
@@ -36,11 +55,11 @@ int main() {
     total_labor_cost = BASE_LABOR_COST * total_labor_hour;
     total_cost = total_paint_cost + total_labor_cost;
 
-    cout << "\n" << "Total Gallons of Paint: " << total_paint_gallon << " gallons" << "\n";
-    cout << "Total Hours of Labor: " << total_labor_hour << " hours" << "\n";
-    cout << "Total Cost of Paint: $" << total_paint_cost << "\n";
-    cout << "Total Cost of Labor: $" << total_labor_cost << "\n";
-    cout << "Total Paint Job Cost: $" << total_cost << "\n\n";
+    cout << "\nTotal Gallons of Paint: " << total_paint_gallon << " gallons\n";
+    cout << "Total Hours of Labor: " << total_labor_hour << " hours\n";
+    cout << "Total Cost of Paint: $" << total_paint_cost << endl;
+    cout << "Total Cost of Labor: $" << total_labor_cost << endl;
+    cout << "Total Paint Job Cost: $" << total_cost << endl;
 
     return 0;
 }   
--- CSCI110/labs/lab3/lab3b.cpp
+++ CSCI110/labs/lab3/lab3b.cpp
@@ -1,4 +1,16 @@
+/*
+Prolog
+    a. Program Description: Guess the Number. This program generates a random number 1-50 and the user needs to guess the correct number. 
+    b. Author: Brandon Tsai
+    c. Date/Time: 01/30/2025
+    d. Input Variables: number
+    e. Process Flow: The program uses srand() and rand() to generat a random number 1-50 and then the user inputs their answer. If the answer is to high or too low the program will let the user know. Once the answer is guessed correctly then the program ends and displays guess_count.
+    f. Output Variables: guess_count
+*/
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -9,7 +21,7 @@ int main () {
 
     srand(time(0));
 
-    int random_num = rand() % 51;
+    int random_num = rand() % 50 + 1;
     int guess_count = 0;
     string guess = "wrong";
 
@@ -20,19 +32,19 @@ int main () {
         cin >> number;
 
         if (number > random_num) {
-            cout << "Your guess was too high! Try Again" << "\n\n";
+            cout << "Your guess was too high! Try Again\n\n";
             guess_count++;
         } else if (number < random_num) {
-            cout << "Your guess was too low! Try Again" << "\n\n";
+            cout << "Your guess was too low! Try Again\n\n";
             guess_count++;
         } else {
-            cout << "You guessed correctly!" << "\n\n";
+            cout << "You guessed correctly!\n\n";
             guess_count++;
             guess = "right";
         }
     }
 
-    cout << "Total Number of Guesses: " << guess_count << "\n\n";
+    cout << "Total Number of Guesses: " << guess_count << endl;
     
     return 0;
 }
