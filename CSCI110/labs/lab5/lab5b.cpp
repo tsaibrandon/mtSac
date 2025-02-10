43cdfff82d3b3a312a9709cacc5c9843b4cc3cd4
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-02-10 11:07:22 -0800

    started working on lab 5 and b

--- CSCI110/labs/lab5/lab5b.cpp
+++ CSCI110/labs/lab5/lab5b.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -7,5 +9,33 @@ void handle_winch(int sig) {}
 int main() {
     signal(SIGWINCH, handle_winch);
 
+    string name;
+    double value;
+    string input_file;
+    string output_file;
+    ifstream in_file;
+    ofstream out_file;
     
+    cout << "Please enter the input file name: ";
+    cin >> input_file;
+    in_file.open(input_file.c_str());
+    if (in_file.fail()) {
+        cout << "File not found" << endl;
+        return 1;
+    }
+    
+    cout << "Please enter the input file name: ";
+    cin >> output_file;
+    out_file.open(output_file.c_str());
+    while (in_file >> name >> value) {
+        cout << "Name is " << name << " and value is " << value << endl;
+        out_file << "Name is " << name << " and value is " << value << endl;
+    }
+    
+    in_file.close();
+    out_file.close();
+    
+    return 0;
 }
+
+
