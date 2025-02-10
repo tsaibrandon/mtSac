d0be773f70029ebd70938c53605ad8d852c370cc
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-02-10 11:08:39 -0800

    started report 5

---
+++ CSCI110/reports/report5/exercise10.3/report5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    double value;
+    string filename;
+    ifstream in_file;
+    ofstream out_file;
+    
+    cout << "Please enter the input file name: ";
+    cin >> filename;
+    in_file.open(filename.c_str());
+    if (in_file.fail()) {
+        cout << "File not found" << endl;
+        return 1;
+    }
+    
+    out_file.open("G:\\output.txt");
+    while (in_file >> name >> value) {
+        cout << "Name is " << name << " and value is " << value << endl;
+        out_file << "Name is " << name << " and value is " << value << endl;
+    }
+    
+    in_file.close();
+    out_file.close();
+    
+    return 0;
+}
