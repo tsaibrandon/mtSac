9e51b6bd31032e63b70f0cc4982d9c9cc08e068d
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-02-11 00:52:30 -0800

    completed lab 5 a and b

--- CSCI110/labs/lab5/lab5a.cpp
+++ CSCI110/labs/lab5/lab5a.cpp
@@ -1,22 +1,107 @@
+/*
+Prolog
+    a. Program Description: 
+    b. Author: Brandon Tsai
+    c. Date/Time: 02/10/2025
+    d. Input Variables: int_input, target
+    e. Process Flow: The program asks the user for an int_input and a target value and then throws it into sequential, then bubble, then binary search functions. Then it will print out what index the target was found and how many comparisons each search algorithm did.
+    f. Output Variables: seq_index, seq_comparisons, bin_index, bin_comparisons
+*/
+
 #include <iostream>
 
 using namespace std;
 
 void handle_winch(int sig) {}
 
-//allow for one input with multiple values
-void int_input(int arr[], int max_size, int& size) {
+//one input with multiple values
+int int_input(int arr[], int max_size, int& size) {
     size = 0;
-    while(cin >> arr[size]) {
+    while (size < max_size && cin >> arr[size]) {
         size++;
         if (cin.peek() == '\n') {
             break;
         }
     }
+    return size; 
+}
+
+//sequential search
+int sequential(int arr[], int size, int target, int& comparisons) {
+    comparisons = 0;
+    for (int i = 0; i < size; i++) {
+        comparisons++;
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+//bubble sort
+void bubble(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+//binary search
+int binary(int arr[], int n, int target, int& comparisons) {
+    comparisons = 0;
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        comparisons++;
+        if (arr[mid] == target) {
+            return mid; 
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }
 
 int main() {
-    signal(SIGWINCH, handle_winch);
+    const int MAX_SIZE = 100;
+    int integers[MAX_SIZE]; //32 54 67 29 35 80 115 44 100 65 37 51 70 26 40 105 33 83 112 41 95
+    int size = 0;
+    int target;
+    int seq_comparisons;
+    int seq_index;
+    int bin_comparisons;
+    int bin_index;
 
-    
+    cout << "Enter at least 20 integers: ";
+    int_input(integers, MAX_SIZE, size);
+
+    if (size < 20) {
+        cout << "Error: You must enter at least 20 integers." << endl;
+        return 1;
+    }
+
+    cout << "Enter the value you would like to search for: ";
+    cin >> target;
+
+    seq_index = sequential(integers, size, target, seq_comparisons);
+    cout << "\nSequential Search\n";
+    cout << "Index: " << seq_index << endl;
+    cout << "Number of Comparisons: " << seq_comparisons << endl;
+
+    bubble(integers, size);
+
+    bin_index = binary(integers, size, target, bin_comparisons);
+    cout << "\nBinary Search\n";
+    cout << "Index: " << bin_index << endl;
+    cout << "Number of Comparisons: " << bin_comparisons << endl;
+
+    return 0;
 }
--- CSCI110/labs/lab5/lab5b.cpp
+++ CSCI110/labs/lab5/lab5b.cpp
@@ -1,3 +1,13 @@
+/*
+Prolog
+    a. Program Description: Springfolk Amateur Golf Club Tournament. This program takes a list of names and scores and reads/prints it out.
+    b. Author: Brandon Tsai
+    c. Date/Time: 02/10/2025
+    d. Input Variables: input_file, output_file, second_output
+    e. Process Flow: The program prompts the user for an input_file, output_file, and second_output file. Then with the input_file it reads out all the first_name, last_name, and values. Then it would use an output.txt file to put all the information into a table.
+    f. Output Variables: in_file, out_file, second_file, golf.dat, output.txt
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,32 +19,57 @@ void handle_winch(int sig) {}
 int main() {
     signal(SIGWINCH, handle_winch);
 
-    string name;
+    string first_name;
+    string last_name;
     double value;
     string input_file;
     string output_file;
+    string second_output;
     ifstream in_file;
     ofstream out_file;
+    ofstream second_file;
     
     cout << "Please enter the input file name: ";
-    cin >> input_file;
+    cin >> input_file; // /Users/tsaibrandon/Documents/input.dat
+    
     in_file.open(input_file.c_str());
+    
     if (in_file.fail()) {
         cout << "File not found" << endl;
         return 1;
     }
     
-    cout << "Please enter the input file name: ";
-    cin >> output_file;
+    cout << "Please enter the output file name: ";
+    cin >> output_file; // /Users/tsaibrandon/Documents/golf.dat
+
+    cout << "Please enter another output file name: ";
+    cin >> second_output; // /Users/tsaibrandon/Documents/output.txt
+    
     out_file.open(output_file.c_str());
-    while (in_file >> name >> value) {
-        cout << "Name is " << name << " and value is " << value << endl;
-        out_file << "Name is " << name << " and value is " << value << endl;
+    
+    while (in_file >> first_name >> last_name >> value) {
+        cout << "Their first name is " << first_name << " their last name is " << last_name << "and the value is " << value << endl;
+        out_file << "Their first name is " << first_name << " their last name is " << last_name << "and the value is " << value << endl;
     }
     
     in_file.close();
     out_file.close();
     
+    in_file.open(input_file.c_str());
+    
+    second_file.open(second_output.c_str());
+    second_file << left << setw(20) << "Name" << right << setw(10) << "Score" << endl;
+    second_file << "========================================" << endl;
+    
+    while (in_file >> first_name >> last_name >> value)
+    {
+        second_file << left << setw(20) << (first_name + " " + last_name) << right << setw(10) << value << endl;
+    }
+
+    in_file.close();
+    out_file.close();
+    second_file.close();
+
     return 0;
 }
 
