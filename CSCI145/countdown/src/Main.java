737e9c51fddb8775b9c440484c20630281e1d194
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-03-03 11:34:18 -0800

    compiled and ran program

--- CSCI145/countdown/src/Main.java
+++ CSCI145/countdown/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 10; i >= 0; i--) {
-            System.out.println("\rSeconds remaining: " + i + " ");
+            System.out.print("\rSeconds remaining: " + i + " ");
             Thread.sleep(1000);
         }
 
