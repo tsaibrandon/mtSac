445bb15899917fa7fdd2faa9e738096b1bb9c4e8
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-03-26 10:45:54 -0700

    created a bubble sort and swap method

--- CSCI145/section4/sort-search/src/Sorter.java
+++ CSCI145/section4/sort-search/src/Sorter.java
@@ -1,3 +1,21 @@
 public class Sorter {
 
+    public static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        for (int j = array.length - 1; !sorted && j > 0; j--) {
+            sorted = true;
+            for (int i = 0; i < j; i++) {
+                if(array[i] > array[i + 1]) {
+                    sorted = false;
+                    swap(array, i, i + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] array, int sub1, int sub2) {
+        int tmp = array[sub1];
+        array[sub1] = array[sub2];
+        array[sub2] = tmp;
+    }
 }
