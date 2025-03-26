0b4a59aa0c5902bca636c6b6d4dc342f8c86f583
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-03-26 11:04:36 -0700

    added a selection sort method

--- CSCI145/section4/sort-search/src/Sorter.java
+++ CSCI145/section4/sort-search/src/Sorter.java
@@ -13,9 +13,24 @@ public class Sorter {
         }
     }
 
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            swap(array, i, findMin(array, i));
+        }
+    }
+
     public static void swap(int[] array, int sub1, int sub2) {
         int tmp = array[sub1];
         array[sub1] = array[sub2];
         array[sub2] = tmp;
     }
+
+    private static int findMin(int[] array, int startSub) {
+        int minSub = startSub;
+
+        for (int i = startSub + 1; i < array.length; i++) 
+            if(array[i] < array[minSub]) minSub = i;
+        
+        return minSub;
+    }  
 }
