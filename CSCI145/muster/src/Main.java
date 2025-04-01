44bffffe50bd4a9c59677db31f5906586d672a56
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-03-31 23:24:50 -0700

    added more game functionality and a way to play again

--- CSCI145/muster/src/Main.java
+++ CSCI145/muster/src/Main.java
@@ -3,8 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
 
-        game.play();
-
+        game.startMenu();
+        
+        char choice = game.playPrompt();
+        
+        if(choice == 'n') {
+            System.out.println("\n---- See you later, alligator ----");
+        } else {
+            game.play(choice);
+        }
     }
 }
-
