197d8ecdf73602053a54703b297424424d2e348b
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-04-23 15:51:47 -0700

    initial commit

---
+++ CSCI145/p2_btsai5/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /** game design planning
+         * 
+         * start the game
+         * 
+         * print the instructions first
+         * generate the board
+         * randomly place the vented room, radiation, and weapon that is not 0,0, 1,0, 0,1
+         * 
+         * create the player at 0,0
+         * player knows their position
+         * player take input R L D U
+         * 
+         * player has a scanner with them
+         * scanner automatically scans when they enter a new room 
+         * 
+         * player's bag or hand starts empty
+         * player takes input P to pick up the weapon making hand or bag full
+         * 
+         * print the input prompt
+         * this input is for player movement and input validation happens from the game 
+         * 
+         * once the player makes it back with the weapon to 0,0 game ends
+         * if player enters radiation or vent room game ends
+         * 
+         */
+    }
+}
